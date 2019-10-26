c68a29320a404a8406693405454e33a8e911c5f9
JKM5438 <JAVON_K_MCGILBERRY@HOMEDEPOT.COM>
2019-10-26 18:40:00 -0400

    added a use effect ref for the menu nav

--- src/components/sayless/navigation/menu.js
+++ src/components/sayless/navigation/menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Link} from "gatsby";
 
 
@@ -9,13 +9,22 @@ import {Link} from "gatsby";
 
 
 export default ({ style }) =>  {
+  const menuContainer = useRef();
+  useEffect(()=> {
+    menuContainer.current.addEventListener('touchmove', function(e) {
 
+      e.preventDefault();
+
+    }, false);
+  })
+
+  console.log(menuContainer)
 
   // this function splits the first and last name onto separate lines
 
 
   return (
-      <div style={style} className="menu-container">
+      <div ref={menuContainer} style={style} className="menu-container">
         <div className="menu-items">
           <h1 className="menu-item">
             <Link style={{textDecoration: "none"}} to={`/aboutus`}>The Studio</Link>
