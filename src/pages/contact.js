30f7e75dfc87f71d9f24d81dc30939580fdcf84d
JKM5438 <JAVON_K_MCGILBERRY@HOMEDEPOT.COM>
2019-12-29 03:09:44 -0500

    add gatsby navigate

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { navigate } from 'gatsby-link'
 import get from 'lodash/get'
 import Layout from "../components/layout";
 import Img from 'gatsby-image'
