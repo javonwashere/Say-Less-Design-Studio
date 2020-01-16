a27bc353d719d766db32712e74528b3ad6e91938
JKM5438 <JAVON_K_MCGILBERRY@HOMEDEPOT.COM>
2020-01-16 03:56:08 -0500

    fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react'
 import { Link } from 'gatsby'
+import get from 'lodash/get'
 import './base.css'
 import Container from './container'
 import Navigation from './sayless/navigation/navigation'
 import Footer from '../components/sayless/footer/footer'
 import { Transition } from "react-spring/renderprops-universal";
 import { Helmet } from 'react-helmet';
+import openGraphImg from "../assets/icons/say-less-image-graph.jpg"
 
 class Template extends React.Component {
+
+  
   render() {
     const { location, children } = this.props
-
     let rootPath = `/`
     if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
       rootPath = __PATH_PREFIX__ + `/`
 
     }
 
-    reactHelmet = (
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+    const metadata = get(this, 'props.data.site.siteMetadata.description');
+    console.log(metadata)
+    const posts = get(this, 'props.data.allContentfulBlogPost.edges');
+    const headerText = get(this, 'props.data.allContentfulHomePageText.nodes[0].title.title');
+
+
+    const reactHelmet = (
       <Helmet title={siteTitle} defer={false}>
         <meta content={metadata} name="description" />
         <meta property="og:url" content="https://sayless.studio" />
