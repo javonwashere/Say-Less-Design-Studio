ac688a9e6a34f055b4e4c118cf2827a7982b78fc
javonwashere <javonmcgilberry@gmail.com>
2020-03-29 18:05:21 -0400

    code changes

--- src/components/sayless/case-study-modules/textbox-module/textbox-module.js
+++ src/components/sayless/case-study-modules/textbox-module/textbox-module.js
@@ -1,49 +1,38 @@
 import React from 'react'
-import './textbox-module.css';
+import './textbox-module.css'
 import Triangle from '../../../../assets/icons/triangle.png'
 
-
 export default ({ props, name }) => {
-    let content;
-    const text = props.text.text;
-    if (props.type != "Header") {
-        content = generateHeader(text, name);
-    } else {
-        content = generateQuote(text, name);
-    }
+  let content
+  const text = props.text.text
+  if (props.type != 'Header') {
+    content = generateHeader(text, name)
+  } else {
+    content = generateQuote(text, name)
+  }
 
-    return (
-        <React.Fragment>    
-            {content}
-        </React.Fragment>
-    )
+  return <React.Fragment>{content}</React.Fragment>
 }
 
 const generateHeader = (text, name) => {
-    return (
-        <div className="textbox-wrapper head">
-            <div className="textbox-triangle-header">
-                <img src={Triangle} />
-            </div>
-            <h1>
-                {text}
-            </h1>
-        </div>
-    )
+  return (
+    <div className="textbox-wrapper head">
+      <div className="textbox-triangle-header">
+        <img src={Triangle} />
+      </div>
+      <h1>{text}</h1>
+    </div>
+  )
 }
 
 const generateQuote = (text, name) => {
-    return (
-        <div className="textbox-wrapper">
-            <div className="textbox-triangle-quote">
-                <img src={Triangle} />
-            </div>
-            <h1>
-                {text}
-            </h1>
-            <div className="textbox-client-quote">
-                - {name}
-            </div>
-        </div>
-    )
+  return (
+    <div className="textbox-wrapper">
+      <div className="textbox-triangle-quote">
+        <img src={Triangle} />
+      </div>
+      <h1>{text}</h1>
+      <div className="textbox-client-quote">- {name}</div>
+    </div>
+  )
 }
