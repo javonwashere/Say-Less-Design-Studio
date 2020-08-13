9d2b0477b357539f21eb1c9ddf06cf1b43d92c0a
javonwashere <javonmcgilberry@gmail.com>
2020-08-13 15:14:43 -0400

    image zoom currently disabled

--- src/pages/inspiration.js
+++ src/pages/inspiration.js
@@ -1,30 +1,32 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React, { useState, useEffect } from 'react'
+import { graphql } from 'gatsby'
 import get from 'lodash/get'
-import Layout from "../components/layout";
+import Layout from '../components/layout'
 import Img from 'gatsby-image'
-import { Link } from "gatsby";
+import { Link } from 'gatsby'
 import './inspiration.css'
 import Masonry from 'react-masonry-css'
 import Zoom from 'react-medium-image-zoom'
 import 'react-medium-image-zoom/dist/styles.css'
-
-
+import { divide } from 'lodash'
 
 class Inspiration extends React.Component {
   render() {
     // console.log(get(this, "props"));
-    const inspoPageData = get(this, "props.data.allContentfulInspirationPage.nodes[0]");
+    const inspoPageData = get(
+      this,
+      'props.data.allContentfulInspirationPage.nodes[0]'
+    )
     const { title, masonryGrid } = inspoPageData
 
-    console.log(masonryGrid);
+    console.log(masonryGrid)
 
     const images = () => {
       return masonryGrid.map(image => {
         return (
-          <Zoom>
-            <Img class="inspiration-zoom-class" fluid={image.fluid} />
-          </Zoom>
+          // <Zoom>
+          <Img class="inspiration-zoom-class" fluid={image.fluid} />
+          // </Zoom>
         )
       })
     }
@@ -32,31 +34,33 @@ class Inspiration extends React.Component {
     return (
       <Layout>
         <div className="inspiration-container">
-          <div className="inspiration-title"><h1>{title}.</h1></div>
+          <div className="inspiration-title">
+            <h1>{title}.</h1>
+          </div>
           <Masonry
             breakpointCols={3}
             className="my-masonry-grid"
-            columnClassName="my-masonry-grid_column">
+            columnClassName="my-masonry-grid_column"
+          >
             {images()}
           </Masonry>
         </div>
-      </Layout >
+      </Layout>
     )
   }
 }
 
-
-export default Inspiration;
+export default Inspiration
 
 export const pageQuery = graphql`
-query {
+  query {
     allContentfulInspirationPage {
       nodes {
         title
         masonryGrid {
           fluid {
             ...GatsbyContentfulFluid_tracedSVG
-        }
+          }
         }
       }
     }
